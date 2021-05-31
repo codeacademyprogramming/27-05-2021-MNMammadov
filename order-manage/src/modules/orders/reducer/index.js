8c9cf5852f1af1dabba91b62886ac260ba4df398
MNMammadov <mehdinm@code.edu.az>
2021-05-31 05:54:50 +0400

    Added initalState for orders

--- order-manage/src/modules/orders/reducer/index.js
+++ order-manage/src/modules/orders/reducer/index.js
@@ -1,6 +1,34 @@
 import { ORDER_ACTIONS } from "../actions/consts";
+import { v4 as uuidv4 } from 'uuid';
 
-export function orderReducer(state = [], action) {
+const initialState = [
+    {
+        id: uuidv4(),
+        tableNo: 1,
+        coffeeType: "Americano",
+        numbOfCoffee: 2,
+        specialNote: "Tund olsun",
+        status: "CREATED"
+    },
+    {
+        id: uuidv4(),
+        tableNo: 2,
+        coffeeType: "Mocha",
+        numbOfCoffee: 5,
+        specialNote: "Meslehetli olsun",
+        status: "IN_PROGRESS"
+    },
+    {
+        id: uuidv4(),
+        tableNo: 5,
+        coffeeType: "Lungo",
+        numbOfCoffee: 5,
+        specialNote: "Meslehetli olsun",
+        status: "DONE"
+    },
+]
+
+export function orderReducer(state = initialState, action) {
     switch (action.type) {
         case ORDER_ACTIONS.ADD_ORDER:
             return [...state, action.payload];
